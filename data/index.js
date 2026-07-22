3d362b0f097921612cf97ce478e018ed219791da
tulenhkan2 <NDKHANG101@GMAIL.COM>
2026-07-22 16:53:53 +0700

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,13 @@
+import { lop4 } from './lop4.js';
+import { lop5 } from './lop5.js';
+import { lop6 } from './lop6.js';
+import { lop7 } from './lop7.js';
+import { lop8 } from './lop8.js';
+
+export const mathData = {
+  lop4,
+  lop5,
+  lop6,
+  lop7,
+  lop8
+};
