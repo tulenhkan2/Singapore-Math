042b00885907122f8bd10c950051acf7a2390422
tulenhkan2 <NDKHANG101@GMAIL.COM>
2026-07-22 17:04:37 +0700

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,13 +1,6 @@
 import { lop4 } from './lop4.js';
-import { lop5 } from './lop5.js';
-import { lop6 } from './lop6.js';
-import { lop7 } from './lop7.js';
-import { lop8 } from './lop8.js';
+
 
 export const mathData = {
-  lop4,
-  lop5,
-  lop6,
-  lop7,
-  lop8
+  lop4
 };
